d2d726b0a349db1a3cf602948a99409984618f78
Abdukulov Talgat <abdukulov.talgat@gmail.com>
2022-04-23 21:24:29 +0500

    Модальные окна

---
+++ src/js/modal.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector('.feedback__button');
+const feedback = document.querySelector('.modal-feedback');
+const overlay = document.querySelector('.modal-overlay');
+
+const modalOpen = 'modal-open';
+
+function initModals() {
+    btn.addEventListener('click', () => {
+        feedback.classList.toggle(modalOpen);
+        overlay.classList.toggle(modalOpen);
+    });
+
+    overlay.addEventListener('click', () => {
+        overlay.classList.toggle(modalOpen);
+        document.querySelectorAll('.modal').forEach((m) => m.classList.remove(modalOpen));
+    });
+
+    document.querySelectorAll('.modal__close').forEach((c) => {
+        c.addEventListener('click', () => {
+            c.parentElement.classList.remove(modalOpen);
+        })
+    });
+}
+
+
+
+
+export default initModals;
--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 import popups from './popups.js';
-
+import modal from './modal.js'
 import swiper from './swiper.js';
 
 popups();
 swiper();
+modal();
 
